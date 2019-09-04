ec37357f9d46944434de5202e710339b66e51a61
collideorscopeapps <mauro81@gmail.com>
2019-09-04 17:20:48 +0200

    added vie controller2

---
+++ MyFirstIphoneApp/ViewController2.h
@@ -0,0 +1,17 @@
+//
+//  ViewController2.h
+//  MyFirstIphoneApp
+//
+//  Created by Mojave-NB126 on 04/09/2019.
+//  Copyright © 2019 collideorscopeapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController2 : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
