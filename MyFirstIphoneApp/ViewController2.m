a6f307782ea3b72385ec4b24e9942aa931988810
collideorscopeapps <mauro81@gmail.com>
2019-09-06 10:16:27 +0200

    Aggiunta table view. Corretto errore firma metodo Aggiunte "region"

--- MyFirstIphoneApp/ViewController2.m
+++ MyFirstIphoneApp/ViewController2.m
@@ -8,45 +8,67 @@
 
 #import "ViewController2.h"
 
-@interface ViewController2 ()
+@interface ViewController2 () {
+    
+    NSMutableArray *people;
+}
 
 @end
 
 @implementation ViewController2
 
-- (NSInteger) numberOfSelectionsInTableView:(UITableView *)tableView {
+#pragma mark - Methods
+
+- (void)loadPeople {
+
+    people = [NSMutableArray arrayWithObjects:@"Mario", @"Antonio", @"Franco", @"Luigi", nil];
+    
+    [tabella reloadData];
+}
+
+#pragma mark - TableView Datasource and Delegate
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+ 
+    return @"ELENCO PERSONE";
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
  
     return 1;
 }
 
-- (NSInteger) tableView:(UITableView *)tableView
-  numberOfRowsInSection:(NSInteger)section{
-    
-    return 1;
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+ 
+    return people.count;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView
-         callForRowAtIndexPath:(NSIndexPath *)indexPath {
-
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellID" forIndexPath:indexPath];
     
-    cell.textLabel.text = @"CIAO";
+    NSString *nome = people[indexPath.row];
+    
+    cell.textLabel.text = nome;
+    
+    
     return cell;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self loadPeople];
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
